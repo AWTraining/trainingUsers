562d78e1d6b9857bdabe7f6f03b9dfaed7985327
sbertone333 <samuel.bertone@edu.itspiemonte.it>
2021-02-19 10:56:50 +0100

    update

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/users', async (req, res) => {
   if (req.body.ip_address) params.ip_address = req.body.ip_address
   if (req.body.birthdate) params.birthdate = req.body.birthdate
   if (req.body.gender) params.gender = req.body.gender
-  
+
 
   await pool.query('INSERT INTO users SET ?', params)
 
@@ -91,7 +91,7 @@ app.put('/users/:id', async (req, res) => {
   try {
     await pool.query('UPDATE users SET ? WHERE id =\'' + currentUserid + '\'', params)
   } catch (error) {
-      res.send('missing params', 500)
+    res.send('missing params', 500)
   }
 
 
@@ -190,21 +190,23 @@ app.get('/user/:id/following', async (req, res) => {
 
 
 //segui un amico
-app.post('/user/:idFollower/follow/:idFollowing', async (req, res) => {   
-  let followerId = req.params.idFollower   
-  let followingId = req.params.idFollowing   
-  if(followerId === followingId){     
-    res.send("Non puoi followarti da solo")   }   
-    else {     
-      let existQ = await pool.query(`SELECT idUserFollowing, idUserFollower FROM test.followers WHERE idUserFollowing = ${followingId} AND idUserFollower = ${followerId}`)       
-      if(existQ[0].length === 0){         
-        await pool.query(`INSERT INTO followers (idUserFollowing, idUserFollower ) VALUES (${followingId}, ${followerId})`)   
-        res.send("a posto")      }      
-        else{       
-          res.send("Già followi sta persona")     
-         }   
-        } 
-      })
+app.post('/user/:idFollower/follow/:idFollowing', async (req, res) => {
+  let followerId = req.params.idFollower
+  let followingId = req.params.idFollowing
+  if (followerId === followingId) {
+    res.send("Non puoi followarti da solo")
+  }
+  else {
+    let existQ = await pool.query(`SELECT idUserFollowing, idUserFollower FROM test.followers WHERE idUserFollowing = ${followingId} AND idUserFollower = ${followerId}`)
+    if (existQ[0].length === 0) {
+      await pool.query(`INSERT INTO followers (idUserFollowing, idUserFollower ) VALUES (${followingId}, ${followerId})`)
+      res.send("a posto")
+    }
+    else {
+      res.send("Già followi sta persona")
+    }
+  }
+})
 
 //smetti di seguire un amico
 app.delete('/user/:idUserFollower/follow/:idUserFollowing', async (req, res) => {
@@ -234,50 +236,50 @@ app.get('/user/:id/followers/tag/:tag', async (req, res) => {
 //lista utenti
 app.get('/users/page/:page', async (req, res) => {
   let page = req.params.page - 1
-  let result1;
+  let result;
   let countQ = await pool.query(`SELECT COUNT(*) FROM test.users`)
   console.log(countQ[0][0])
-   if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-     return res.send('page not found', 404) 
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
+  }
+  else {
+    if (page < 1) {
+      result = await pool.query(`SELECT * FROM test.users ORDER BY id LIMIT ${page}, 20`)
     }
-  else{
-     if(page < 1){
-    result1 = await pool.query(`SELECT * FROM test.users ORDER BY id LIMIT ${page}, 20`)
+    else {
+      page = page * 20;
+      result = await pool.query(`SELECT * FROM test.users ORDER BY id LIMIT ${page}, 20`)
+    }
+    let response = result[0]
+    res.send({
+      users: response
+    })
   }
-  else{
-    page = page * 20;
-    result1 = await pool.query(`SELECT * FROM test.users ORDER BY id LIMIT ${page}, 20`)
-  }
-  let response1 = result1[0]
-  res.send({
-  users: response1
-  })
-}
 })
 
 //lista utenti data la città
-app.get('/user/city/:city/page/:page', async (req, res) => {
+app.get('/users/city/:city/page/:page', async (req, res) => {
   let page = req.params.page - 1
   let city = req.params.city
   let countQ = await pool.query(`SELECT COUNT(*) FROM test.users INNER JOIN test.cities ON test.users.id_city = test.cities.id WHERE city = "${city}"`)
   let result
-  if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-    return res.send('page not found', 404) 
-   }
- else{
-  if (page < 1){
-    result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.cities ON test.users.id_city = test.cities.id WHERE city = "${city}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
   }
-  else{
-    page = page * 20;
-    result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.cities ON test.users.id_city = test.cities.id WHERE city = "${city}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  else {
+    if (page < 1) {
+      result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.cities ON test.users.id_city = test.cities.id WHERE city = "${city}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    else {
+      page = page * 20;
+      result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.cities ON test.users.id_city = test.cities.id WHERE city = "${city}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    console.log(result[0][0])
+    let response = result[0][0]
+    res.send({
+      users : response
+    })
   }
-  console.log(result[0][0])
-  let response = result[0][0]
-  res.send({
-    user: response
-  })
-}
 })
 
 //lista utenti registrati negli ultimi x mesi
@@ -286,69 +288,69 @@ app.get('/users/months/:months/page/:page', async (req, res) => {
   let page = req.params.page - 1
   let result;
   let countQ = await pool.query(`SELECT COUNT(*) FROM test.users WHERE (DATE_SUB(CURDATE(),INTERVAL 2 MONTH) < users.signup_date)`)
-  if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-    return res.send('page not found', 404) 
-   }
- else{
-  if (page < 1){
-    result = await pool.query(`SELECT first_name, last_name, signup_date FROM test.users WHERE (DATE_SUB(CURDATE(),INTERVAL ${months} MONTH) < users.signup_date) ORDER BY test.users.id LIMIT ${page}, 20`)
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
   }
   else {
-    result = await pool.query(`SELECT first_name, last_name, signup_date FROM test.users WHERE (DATE_SUB(CURDATE(),INTERVAL 2 MONTH) < users.signup_date) ORDER BY test.users.id LIMIT ${page}, 20`)
+    if (page < 1) {
+      result = await pool.query(`SELECT first_name, last_name, signup_date FROM test.users WHERE (DATE_SUB(CURDATE(),INTERVAL ${months} MONTH) < users.signup_date) ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    else {
+      result = await pool.query(`SELECT first_name, last_name, signup_date FROM test.users WHERE (DATE_SUB(CURDATE(),INTERVAL 2 MONTH) < users.signup_date) ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    let response = result[0]
+    res.send({
+      users : response
+    })
   }
-  let response = result[0]
-  res.send({
-    user: response
-  })
-}
 })
 
 
 //  Lista utenti che hanno più di x anni
-app.get('/user/years/:years/page/:page', async (req, res) => {
+app.get('/users/years/:years/page/:page', async (req, res) => {
   let years = req.params.years
   let page = req.params.page - 1
   let result;
   let countQ = await pool.query(`SELECT COUNT(*) FROM test.users WHERE ((DATEDIFF(CURDATE(), test.users.birthdate)/365.25) > ${years})`)
-  if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-    return res.send('page not found', 404) 
-   }
- else{
-  if (page < 1){
-    result = await pool.query(`SELECT first_name, last_name, birthdate FROM test.users WHERE ((DATEDIFF(CURDATE(), test.users.birthdate)/365.25) > ${years}) ORDER BY test.users.id LIMIT ${page}, 20`)
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
   }
   else {
-    result = await pool.query(`SELECT first_name, last_name, birthdate FROM test.users WHERE ((DATEDIFF(CURDATE(), test.users.birthdate)/365.25) > ${years}) ORDER BY test.users.id LIMIT ${page}, 20`)
+    if (page < 1) {
+      result = await pool.query(`SELECT first_name, last_name, birthdate FROM test.users WHERE ((DATEDIFF(CURDATE(), test.users.birthdate)/365.25) > ${years}) ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    else {
+      result = await pool.query(`SELECT first_name, last_name, birthdate FROM test.users WHERE ((DATEDIFF(CURDATE(), test.users.birthdate)/365.25) > ${years}) ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    let response = result[0]
+    res.send({
+      users: response
+    })
   }
-  let response = result[0]
-  res.send({
-    user: response
-  })
-}
 })
 
 // lista utenti dato il sesso
-app.get('/user/gender/:gender/page/:page', async (req, res) => {
+app.get('/users/gender/:gender/page/:page', async (req, res) => {
   let gender = req.params.gender
   let page = req.params.page - 1
   let result;
   let countQ = await pool.query(`SELECT COUNT(*) FROM test.users WHERE gender = "${gender}"`)
-  if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-    return res.send('page not found', 404) 
-   }
- else{
-  if (page < 1){
-    result = await pool.query(`SELECT first_name, last_name FROM users WHERE gender = "${gender}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
   }
-  else{
-    result = await pool.query(`SELECT first_name, last_name FROM users WHERE gender = "${gender}" ORDER BY test.users.id LIMIT ${page}, 20`)
-  }
+  else {
+    if (page < 1) {
+      result = await pool.query(`SELECT first_name, last_name FROM users WHERE gender = "${gender}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    else {
+      result = await pool.query(`SELECT first_name, last_name FROM users WHERE gender = "${gender}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
     console.log(result[0])
-  let response = result[0]
-  res.send({
-    user: response
-  })
-}
+    let response = result[0]
+    res.send({
+      users : response
+    })
+  }
 })
 
 
@@ -358,101 +360,101 @@ app.get('/legalUsers/carmodel/:carmodel/page/:page', async (req, res) => {
   let page = req.params.page - 1
   let result
   let countQ = await pool.query(`SELECT COUNT(*) FROM test.users INNER JOIN test.car_models ON test.users.id_car_model = test.car_models.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND car_model = "${carmodel}"`)
-  if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-    return res.send('page not found', 404) 
-   }
- else{
-  if (page < 1){
-    result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.car_models ON test.users.id_car_model = test.car_models.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND car_model = "${carmodel}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
   }
-  else{
-    result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.car_models ON test.users.id_car_model = test.car_models.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND car_model = "${carmodel}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  else {
+    if (page < 1) {
+      result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.car_models ON test.users.id_car_model = test.car_models.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND car_model = "${carmodel}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    else {
+      result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.car_models ON test.users.id_car_model = test.car_models.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND car_model = "${carmodel}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    console.log(result[0][0])
+    let response = result[0][0]
+    res.send({
+      users : response
+    })
   }
-  console.log(result[0][0])
-  let response = result[0][0]
-  res.send({
-      user: response
-  })
-}
 })
 
 //Lista utenti Maggiorenni registrati da più di x mesi che vivono in uno stato X
 app.get('/legalUsers/country/:country/page/:page', async (req, res) => {
-let country = req.params.country
-let page = req.params.page - 1
-let result;
+  let country = req.params.country
+  let page = req.params.page - 1
+  let result;
   let countQ = await pool.query(`SELECT COUNT(*) FROM test.users INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND country = "${country}"`)
-  if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-    return res.send('page not found', 404) 
-   }
- else{
-  if (page < 1){
-    result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND country = "${country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
   }
-else {
-  result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND country = "${country}" ORDER BY test.users.id LIMIT ${page}, 20`)
-   }
-console.log(result[0][0])
-let response = result[0][0]
-res.send({
-    user: response
+  else {
+    if (page < 1) {
+      result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND country = "${country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    else {
+      result = await pool.query(`SELECT first_name, last_name FROM users INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE YEAR(CURDATE()) - YEAR(birthdate) > 18 AND country = "${country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    console.log(result[0][0])
+    let response = result[0][0]
+    res.send({
+      users : response
+    })
+  }
 })
-}
-})  
-  
+
 
 
 //Elenco di tutte le città data la country in ingresso
-app.get('/user/country/:country/page/:page', async (req, res) => {
+app.get('/cities/country/:country/page/:page', async (req, res) => {
   let country = req.params.country
   let page = req.params.page - 1
   let result
   let countQ = await pool.query(`SELECT COUNT(*) FROM cities INNER JOIN test.users ON test.cities.id = test.users.id_city INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE test.countries.country = "${country}"`)
-  if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-    return res.send('page not found', 404) 
-   }
- else{
-  if (page < 1){
-    result = await pool.query(`SELECT city FROM cities INNER JOIN test.users ON test.cities.id = test.users.id_city INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE test.countries.country = "${country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
   }
   else {
-    result = await pool.query(`SELECT city FROM cities INNER JOIN test.users ON test.cities.id = test.users.id_city INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE test.countries.country = "${country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    if (page < 1) {
+      result = await pool.query(`SELECT city FROM cities INNER JOIN test.users ON test.cities.id = test.users.id_city INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE test.countries.country = "${country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    else {
+      result = await pool.query(`SELECT city FROM cities INNER JOIN test.users ON test.cities.id = test.users.id_city INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE test.countries.country = "${country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    console.log(result[0])
+    let response = result[0]
+    res.send({
+      cities : response
+    })
   }
-  console.log(result[0])
-  let response = result[0]
-  res.send({
-      user: response
-  })
-}
 })
 
 //Elenco di tutti i modelli di auto possedute in una country
-app.get('/car_model/Country/:Country/page/:page', async (req, res) => {
+app.get('/car_models/Country/:Country/page/:page', async (req, res) => {
   let Country = req.params.Country
   let page = req.params.page - 1
   let result
   let countQ = await pool.query(`SELECT COUNT(*) FROM test.car_models INNER JOIN test.users ON test.users.id_car_model = test.car_models.id INNER JOIN test.countries ON  test.users.id_country = test.countries.id WHERE test.countries.country = "${Country}"`)
   console.log(countQ[0][0]["COUNT(*)"])
   console.log(`SELECT COUNT(*) FROM car_models INNER JOIN test.users ON test.car_models.id = test.users.id_car_model INNER JOIN test.countries ON test.users.id_country = test.countries.id WHERE test.car_models.car_model = "${Country}"`)
-  if(req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"]/20)){   
-    return res.send('page not found', 404) 
-   }
- else{
-  if (page < 1){
-    result = await pool.query(`SELECT car_model FROM test.car_models INNER JOIN test.users ON test.users.id_car_model = test.car_models.id INNER JOIN test.countries ON  test.users.id_country = test.countries.id WHERE test.countries.country = "${Country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  if (req.params.page > Math.ceil(countQ[0][0]["COUNT(*)"] / 20)) {
+    return res.send('page not found', 404)
   }
-  else{
-    result = await pool.query(`SELECT car_model FROM test.car_models INNER JOIN test.users ON test.users.id_car_model = test.car_models.id INNER JOIN test.countries ON  test.users.id_country = test.countries.id WHERE test.countries.country = "${Country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+  else {
+    if (page < 1) {
+      result = await pool.query(`SELECT car_model FROM test.car_models INNER JOIN test.users ON test.users.id_car_model = test.car_models.id INNER JOIN test.countries ON  test.users.id_country = test.countries.id WHERE test.countries.country = "${Country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    else {
+      result = await pool.query(`SELECT car_model FROM test.car_models INNER JOIN test.users ON test.users.id_car_model = test.car_models.id INNER JOIN test.countries ON  test.users.id_country = test.countries.id WHERE test.countries.country = "${Country}" ORDER BY test.users.id LIMIT ${page}, 20`)
+    }
+    console.log(result[0])
+    let response = result[0]
+    res.send({
+      car_models: response
+    })
   }
-  console.log(result[0])
-  let response = result[0]
-  res.send({
-      user: response
-  })
-}
-}) 
+})
 
-  
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
